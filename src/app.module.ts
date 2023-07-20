27e6b5e2971e9557dab657c90367caed27580867
Gasello <9yar9@mail.ru>
2023-07-20 11:17:32 +0300

    nest init

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'vzcjtl',
+      database: 'Word-library',
+      models: [],
+    }),
+  ],
+})
+export class AppModule {}
