f8c02aa6e02c2b08903c696a90d948c0bb7a6a07
Gasello <9yar9@mail.ru>
2023-07-21 14:40:09 +0300

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'C:\\work\\words-library_back\\.env'
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'vzcjtl',
-      database: 'Word-library',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
+      autoLoadModels: true,
     }),
+    UsersModule,
   ],
+  providers: [UsersService],
 })
 export class AppModule {}
